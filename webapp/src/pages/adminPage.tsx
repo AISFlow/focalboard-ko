4f8257af6c21d2759354fa80eaec019230444ee0
Nastasia <lisitsinan@gmail.com>
2025-01-06 17:17:35 +0200

    fix: add board works at admin page

--- webapp/src/pages/adminPage.tsx
+++ webapp/src/pages/adminPage.tsx
@@ -1,15 +1,17 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {useEffect, useState} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 
 import './adminPage.scss'
 import client from '../octoClient'
 import {IUser} from '../user';
 import Sidebar from '../components/sidebar/sidebar';
+import BoardTemplateSelector from '../components/boardTemplateSelector/boardTemplateSelector';
 
 const AdminPage = () => {
 
     const [users, setUsers] = useState<IUser[]>([]);
+    const [boardTemplateSelectorOpen, setBoardTemplateSelectorOpen] = useState<boolean>(false)
 
     const fetchUsers = async () => {
         const result = await client.getTeamUsers();
@@ -20,21 +22,32 @@ const AdminPage = () => {
         fetchUsers();
     }, []);
 
+    const openBoardTemplateSelector = useCallback(() => {
+        setBoardTemplateSelectorOpen(true)
+    }, [])
+    const closeBoardTemplateSelector = useCallback(() => {
+        setBoardTemplateSelectorOpen(false)
+    }, [])
+
     return (
         <div className='AdminPage'>
-            <Sidebar onBoardTemplateSelectorOpen={function (): void {
-                throw new Error('Function not implemented.');
-            } }/>
+            <Sidebar
+                onBoardTemplateSelectorOpen={openBoardTemplateSelector}
+                onBoardTemplateSelectorClose={closeBoardTemplateSelector}
+            />
             <div className='panel'>
-            <h1 className='tite'>Team Users</h1>
-            <p>Users count: {users.length}</p>
-                <ul className='users-list'>
-                    {users.map((user) => (
-                        <li key={user.id}>
-                        {user.username}
-                        </li>
-                    ))}
-            </ul>
+                {boardTemplateSelectorOpen &&
+                    <BoardTemplateSelector onClose={closeBoardTemplateSelector}/>
+                }
+                <h1 className='tite'>Team Users</h1>
+                <p>Users count: {users.length}</p>
+                    <ul className='users-list'>
+                        {users.map((user) => (
+                            <li key={user.id}>
+                            {user.username}
+                            </li>
+                        ))}
+                </ul>
             </div>
         </div>
     );
