b602ac45bf4ac83411f8f72013a023b55e9b1756
Nastasia <lisitsinan@gmail.com>
2025-01-06 21:45:29 +0200

    feat: add button

--- webapp/src/pages/adminPage.tsx
+++ webapp/src/pages/adminPage.tsx
@@ -1,15 +1,23 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import React, {useCallback, useEffect, useState} from 'react'
+import {useIntl} from 'react-intl';
 
 import './adminPage.scss'
 import client from '../octoClient'
 import {IUser} from '../user';
 import Sidebar from '../components/sidebar/sidebar';
 import BoardTemplateSelector from '../components/boardTemplateSelector/boardTemplateSelector';
+import User from '../components/user/user';
+
+import {useAppSelector} from '../store/hooks';
+import {getMe} from '../store/users';
+import {getClientConfig} from '../store/clientConfig';
+import {ClientConfig} from '../config/clientConfig';
 
 const AdminPage = () => {
-
+    const clientConfig = useAppSelector<ClientConfig>(getClientConfig)
+    const me = useAppSelector<IUser|null>(getMe)
     const [users, setUsers] = useState<IUser[]>([]);
     const [boardTemplateSelectorOpen, setBoardTemplateSelectorOpen] = useState<boolean>(false)
 
@@ -39,14 +47,20 @@ const AdminPage = () => {
                 {boardTemplateSelectorOpen &&
                     <BoardTemplateSelector onClose={closeBoardTemplateSelector}/>
                 }
-                <h1 className='tite'>Team Users</h1>
-                <p>Users count: {users.length}</p>
-                    <ul className='users-list'>
-                        {users.map((user) => (
-                            <li key={user.id}>
-                            {user.username}
-                            </li>
-                        ))}
+                <h1 className='ml-3'>Team Users</h1>
+                { users.length > 0 && (
+                    <h2 className='users-count ml-3'>Users count: {users.length}</h2>
+                )}
+                <ul className='users-list ml-3'>
+                    {users.map((user) => (
+                        <li key={user.id} className='user-item'>
+                            <User
+                                user={user}
+                                teammateNameDisplay={clientConfig.teammateNameDisplay}
+                                isMe={me && user.id === me.id}
+                            />
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
